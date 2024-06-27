d9e2f628b6c32c5fd07aeb029e19bca43f5ae68a
tiski <lfhmztshkn@gmail.com>
2024-06-27 18:05:54 +0300

    fix toggle menu

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,11 +1,30 @@
 export const menuToggle = () => {
     const menuIcon = document.getElementById('menu-icon');
     const navLinks = document.getElementById('nav-links');
+    const menuWrapper = document.getElementById('menu-wrapper')
 
-    menuIcon.addEventListener('click', () => {
+    menuIcon.addEventListener('click', (e) => {
+        e.stopPropagation();
         navLinks.classList.toggle('active');
+        menuWrapper.classList.toggle('active');
         menuIcon.innerHTML = navLinks.classList.contains('active')
             ? '<img src="/assets/icons/close-menu-icon.svg" alt="Close Icon">'
             : '<img src="/assets/icons/menu-icon.svg" alt="Menu Icon">';
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!menuWrapper.contains(e.target) && menuWrapper.classList.contains('active')) {
+            menuWrapper.classList.remove('active');
+            navLinks.classList.remove('active');
+            menuIcon.innerHTML = '<img src="/assets/icons/menu-icon.svg" alt="Menu Icon">';
+        }
+    });
+
+    navLinks.addEventListener('click', (e) => {
+        if (e.target.tagName === 'A' || e.target.closest('a')) {
+            menuWrapper.classList.remove('active');
+            navLinks.classList.remove('active');
+            menuIcon.innerHTML = '<img src="/assets/icons/menu-icon.svg" alt="Menu Icon">';
+        }
     });
 };
