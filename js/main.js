af0cddac9e61fcbbb678fb24d4a387b5b9665c7d
tiski <lfhmztshkn@gmail.com>
2024-06-30 04:47:52 +0300

    fix file path

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import {loadSection} from "./js/loader.js";
-import {initializeDefaultChip, initializeGallery} from "./js/gallery.js";
-import {swapIcons} from "./js/swapIcons.js";
-import {menuToggle} from "./js/menuToggle.js";
-import {smoothScroll} from "./js/smoothScroll.js";
+import {loadSection} from "./loader.js";
+import {initializeDefaultChip, initializeGallery} from "./gallery.js";
+import {swapIcons} from "./swapIcons.js";
+import {menuToggle} from "./menuToggle.js";
+import {smoothScroll} from "./smoothScroll.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     loadSection('sections/header.html', 'header', () => {
